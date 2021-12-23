586059810a0d3719ad2ea50f6582baf0913f8d02
Matej Horvat <matej@horvat.si>
2021-12-23 14:27:32 +0100

    Update api path

--- src/routes/users.js
+++ src/routes/users.js
@@ -32,5 +32,5 @@ module.exports = usersRouter => {
     router.post("/register", [registerCheck.checkUniqueUsernameEmail], usersController.register);
 
 
-    usersRouter.use('/api/v1/users', router);
+    usersRouter.use('users/api/v1', router);
 };
